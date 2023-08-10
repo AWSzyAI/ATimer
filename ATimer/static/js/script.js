036c3cc49716880c0c4fb0e10f36074cfab711e9
Szy <szy@nnu.edu.cn>
2023-08-10 14:39:10 +0800

    js

---
+++ ATimer/static/js/script.js
@@ -0,0 +1,39 @@
+// 获取元素
+const newBtn = document.getElementById('new-btn');
+const modal = document.getElementById('new-modal');
+
+// 新建按钮点击事件  
+newBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+
+// 取消按钮点击事件
+const cancelBtn = document.getElementById('cancel-btn');
+cancelBtn.addEventListener('click', () => {
+  modal.style.display = 'none'; 
+})
+
+
+// 确定按钮点击事件
+const okBtn = document.getElementById('ok-btn');
+
+okBtn.addEventListener('click', () => {
+
+  const name = document.getElementById('project-name').value;
+  const status = document.getElementById('project-status').value;
+
+  fetch('/api/projects', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      status
+    })
+  }).then(res => {
+    modal.style.display = 'none';
+    window.location.reload();
+  })
+
+})
