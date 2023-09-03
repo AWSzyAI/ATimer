e042118286ee5b3fa1d9e279865e990483617114
Szy <szy@nnu.edu.cn>
2023-09-03 23:19:25 +0800

    js

--- ATimer-db/ATimer/static/js/script.js
+++ ATimer-db/ATimer/static/js/script.js
@@ -1,22 +1,22 @@
 // static/script.js
-
+/*
 let timerBtn = document.getElementsByClassName('timer-btn');
-let isactive = 'inactive';
+let active = true;
 let startTime = null;
 let projectId = timerBtn.getAttribute('project-id');
 
 timerBtn.addEventListener('click', function() {
-  if (isactive === 'inactive') {
+  if (active) {
     // Start timer
     startTime = new Date();
     print(startTime);
-    isactive = 'active';
+    active = false;
     timerBtn.innerText = 'Stop';
   } else {
     // Stop timer
     let endTime = new Date();
     print(endTime);
-    isactive = 'inactive';
+    active = true;
     timerBtn.innerText = 'Record';
     
     let record = {
@@ -31,6 +31,7 @@ timerBtn.addEventListener('click', function() {
   }
 });
 
+*/
 
 function formatDateTime(dateTime) {
   // 格式化为 YYYY-MM-DD HH:MM:SS 格式
@@ -46,47 +47,119 @@ function formatDateTime(dateTime) {
 function createRecord(record) {
   let xhr = new XMLHttpRequest();
   xhr.open('POST', '/create_record');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        // Record created successfully
-        console.log('Record created:', record);
-      } else {
-        console.error('Failed to create record:', xhr.status);
-      }
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      console.log('Record created:', record);
+    } else {
+      console.error('Failed to create record:', xhr.status);
     }
-  };
+  }
+  
+  xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify(record));
 }
 
+function clickHandler(e) {
+  let btn = e.target;
+  let projectId = btn.id;
+  let active = false;
+  let startTime, endTime;
+  if(!active){
+    startTime = new Date();
+    active = true;
+    btn.innerText = 'Stop';
+  }else{
+    endTime = new Date();
+    active = false;
+    btn.innerText = 'Record';
+
+    let record = {
+      project_id: projectId,
+      start_time: formatDateTime(startTime),
+      end_time: formatDateTime(endTime),
+    };
+    createRecord(record);
+  }
+}
+
+
+// 获取所有计时按钮 
+const timerBtns = document.querySelectorAll('.timer-btn');
+
+
+timerBtns.forEach(btn => {
+  let active = false;//每个按钮维护独立 active状态
+  btn.addEventListener('click', clickHandler);
+});
+
 
 /*
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+
+  const timerBtn = card.querySelector('.timer-btn');
+  //const projectId = timerBtn.getAttribute('project-id');
+  let active = false;
+  let startTime, endTime;
+  let projectId;
+
+  timerBtn.addEventListener('click', function() {
+    projectId = card.getAttribute('data-project-id');
+
+    if(!active){
+      startTime = new Date();
+      active = true;
+      timerBtn.innerText = 'Stop';
+    }else{
+      endTime = new Date();
+      active = false;
+      timerBtn.innerText = 'Record';
+      
+      let record = {
+        project_id: projectId,
+        start_time: formatDateTime(startTime),
+        end_time: formatDateTime(endTime),
+      };
+      createRecord(record);
+    }
+  });
+
+});
+*/
+/*
 let timerBtn = document.getElementById('timer-btn');
-let isactive = 'inactive';
+let active = false;
 let startTime = null;
+//let projectCard = document.getElementsByClassName('project-card');
+//let projectId = projectCard.getAttribute('data-project-id')
+let projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+  let projectId = card.getAttribute('data-project-id');
+});
 
 timerBtn.addEventListener('click', function() {
-  if (isactive === 'inactive') {
+  if (active) {
     // Start timer
     startTime = new Date();
-    isactive = 'active';
+    active = false;
     timerBtn.innerText = 'Stop';
   } else {
     // Stop timer
-    isactive = 'inactive';
-    timerBtn.innerText = 'Record';
     let endTime = new Date();
+    active = true;
+    timerBtn.innerText = 'Record';
+    
     let record = {
       start_time: formatDateTime(startTime),
-      end_time: formatDateTime(endTime)
+      end_time: formatDateTime(endTime),
+      project_id: projectId
     };
     createRecord(record);
     startTime = null;
   }
 });
-
-
 
 
 */
